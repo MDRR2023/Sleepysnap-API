d978bcc55dca2d80f11338bb5d8c4954cbb8f297
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2023-10-11 12:41:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -51,4 +51,4 @@ def predict():
         return jsonify({'error': str(e)}), 400, {'Content-Type': 'application/json'}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0')
